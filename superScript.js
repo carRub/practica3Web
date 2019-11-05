025ffc5cb0fd085786917ce13ff4dfa396d3c033
Carlos <carub96@gmail.com>
2019-11-05 16:26:15 -0600

    First official commit

---
+++ superScript.js
@@ -0,0 +1,14 @@
+window.localStorage.setItem("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHBlZGllbnRlIjoiNzA5Mzg1IiwiaWF0IjoxNTcyMjcxMTAwfQ.Ifaroy08WSikYjZuiKfC-vAVj8s2VF5iXdZR1FCXKhw");
+
+
+var form = document.querySelector('.modal-body form'); 
+
+form.addEventListener("change", validateInput);
+
+function validateInput() {
+    var formList = document.querySelectorAll("form input:invalid");
+    console.table(formList);
+    
+}
+
+console.log(form);
