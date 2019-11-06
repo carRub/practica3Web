63b932b30eb3f7f66bb86f65a9566c41b7f1e12d
Carlos <carub96@gmail.com>
2019-11-05 23:27:06 -0600

    commit to continue in another computer

--- superScript.js
+++ superScript.js
@@ -1,3 +1,6 @@
+"useStrict"
+
+//saving my token
 window.localStorage.setItem("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHBlZGllbnRlIjoiNzA5Mzg1IiwiaWF0IjoxNTcyMjcxMTAwfQ.Ifaroy08WSikYjZuiKfC-vAVj8s2VF5iXdZR1FCXKhw");
 
 
@@ -6,10 +9,34 @@ var form = document.querySelector('.modal-body form');
 form.addEventListener("change", validateInput);
 
 function validateInput() {
-    var formList = document.querySelectorAll("form input:invalid");
-    let arr = Array.from(formList);
-    if(Array)
-    
+    var formInvalidList = document.querySelectorAll("form input:invalid");
+    var formValidList = document.querySelectorAll("form input:valid");
+
+    if(formInvalidList.length > 0){
+        let arr = Array.from(formInvalidList);
+        arr.forEach(markInvalid);
+        
+        
+    }
+    if(formValidList.length > 0){
+        let validArr = Array.from(formValidList);
+        validArr.forEach(removeInvalid);
+        if(formInvalidList.length == 0){
+            console.log("should be enabled");
+            document.querySelector('.modal-body .btn').disabled = false;
+        }
+    }
 }
+
+//for each functions
+function markInvalid(item) {
+    console.log("markInvalid");
+    item.setAttribute("style", "border-color: red;");
+}
+
+function removeInvalid(item){
+    item.removeAttribute("style");
+}
+
 
 console.log(form);
