ac0de545010af371b2e667fcf789eabcf782f234
Carlos <carub96@gmail.com>
2019-11-08 14:24:17 -0600

    complete login request

--- superScript.js
+++ superScript.js
@@ -19,14 +19,20 @@ form.addEventListener("submit", submitForm);
 var submit = document.getElementById("submitRegistry");
 submit.disabled = true;
 
-//passwords
-var password1 = document.getElementById("password1");
-var password2 = document.getElementById("password2");
+
+
+//login 
+var loginButton = document.getElementById("loginButton");
+loginButton.onclick = loginRequest;
 
 //form onchange listener
 function validateInput() {
     var formInvalidList = document.querySelectorAll("form input:invalid");
     var formValidList = document.querySelectorAll("form input:valid");
+
+    //passwords
+    var password1 = document.getElementById("password1");
+    var password2 = document.getElementById("password2");
 
     if(formInvalidList.length > 0){
         let arr = Array.from(formInvalidList);
@@ -54,12 +60,10 @@ function submitForm(event) {
     let inputArr = Array.from(document.querySelectorAll("form input:valid"));
     inputArr.forEach(createInputJson);    
 
-    console.log(`${baseUrl}users/`);
     let xhr = new XMLHttpRequest();
     xhr.open('POST', `${baseUrl}users/`);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.setRequestHeader('x-auth', localStorage.getItem("token"));
-    console.log(xhr.getAllResponseHeaders);
     xhr.send(JSON.stringify(user));
     xhr.onload = function() {
         if(xhr.status != 201){
@@ -70,6 +74,33 @@ function submitForm(event) {
             alert("User registered succesfully"); //
         }
     }
+}
+
+//login onclick handler
+function loginRequest() {
+    let loginUser = {
+        correo: document.getElementById("loginMail").value,
+        password: document.getElementById("loginPassword").value
+    };
+
+    xhr = new XMLHttpRequest();
+    xhr.open('POST', `${baseUrl}login/`);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('x-auth', localStorage.getItem("token"));
+    xhr.send(JSON.stringify(loginUser));
+    xhr.onload = function() {
+        if(xhr.status != 200){
+            //error
+            alert(xhr.status + ': ' + xhr.statusText);
+            console.log(xhr.responseText);
+        }else {
+            //go to consultas
+            window.location = "consulta.html";
+        }
+    }
+
+
+
 }
 
 //forEach functions
